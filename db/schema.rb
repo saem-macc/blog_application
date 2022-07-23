8c472aa7968a1d43cacbb8c10057f66448393306
SAR macc <salvador.aguirre.apps@gmail.com>
2022-07-23 08:59:16 -0500

    until min 12 part 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_13_014218) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_23_125739) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "views", default: 0
   end
 
 end
