4019fe00f40e178825b9569df9b5680018b23e7d
SAR macc <salvador.aguirre.apps@gmail.com>
2022-07-24 06:30:19 -0500

    added rich comment basic

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create(email: "sar@example.com", password: "123123", password_confirmation: "123123")
+usuario = User.create(email: "sar@example.com", password: "123123", password_confirmation: "123123", name: "Salvador")
 
 10.times do |x|
-  Post.create(title: "Title No. #{x}.", body: "Body No. #{x} too much words here...", user_id: User.first.id)
+  Post.create(title: "Title No. #{x}.", body: "Body No. #{x} too much words here...", user_id: usuario.id)
 end
 
